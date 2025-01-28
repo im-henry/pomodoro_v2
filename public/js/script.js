025f02f9f3cf2aae205f907e8fb6b65280e47df8
Henry <hrw13998@gmail.com>
2025-01-28 11:42:03 -0500

    testing changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,10 @@
-if (Notification.permission === 'default') {
-    Notification.requestPermission(); // Solicitar permiso si no está otorgado
-}
+
 
 /**Button by: Copyright (c) 2024 by Aaron Iker (https://codepen.io/aaroniker/pen/abzOdRR) */
 document.querySelectorAll('.play-pause-button').forEach(button => {
     button.addEventListener('click', e => {
         //Sound
-        const audio = new Audio('./media/start_button.mp3');
-        audio.play().catch(error => {
-        console.error("Error playing sound:", error);
-        }); 
+
 
         if(button.classList.contains('playing')) {
             button.classList.remove('paused', 'playing');
@@ -22,3 +17,4 @@ document.querySelectorAll('.play-pause-button').forEach(button => {
         }
     });
 });
+
